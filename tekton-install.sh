b495629573608b2edc8755247dcfe56f8f7b723e
Ubuntu <ubuntu@ip-10-0-1-171.me-south-1.compute.internal>
2020-10-20 18:55:41 +0000

    editing tekton-install file

--- tekton-install.sh
+++ tekton-install.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+kubectl apply --filename https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml
+kubectl create configmap config-artifact-pvc \
+                         --from-literal=size=10Gi \
+                         --from-literal=storageClassName=manual \
+                         -o yaml -n tekton-pipelines \
+                         --dry-run=client | kubectl replace -f -
 sudo apt update
 sudo apt install -y gnupg
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3EFE0E0A2F2F60AA
